b9fba777b2b96df82f34c93a5724d371a0d367a3
USER PC <USER PC@USER>
2015-09-17 09:34:13 +0100

    initial Commit

---
+++ src/job/Job.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job;
+
+/**
+ *
+ * @author USER PC
+ */
+public class Job {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         * ****
+         * JOB POSTING Java Developer
+         * https://planaxis.secure.force.com/FCMS__CMSLayout?jobIds=a0K1200000DOOFnEAP&page=JobDetailPage&sessionId=&jobSite=default&p=Candidate
+         * 1.	JSE – This means Java Standard Edition, it is simply a computing
+         * platform or an environment that supports java software. (
+         * https://en.wikipedia.org/wiki/Java_Platform,_Standard_Edition ) 2.
+         * JDBC – Java Data base connectivity.
+         * http://www.oracle.com/technetwork/java/javase/jdbc/index.html 3.	JPA
+         * – Java Persistence API
+         * https://en.wikipedia.org/wiki/Java_Persistence_API.
+         *
+         * Monthly potential salaries of Java Developers in: Toronto – C$5,995
+         * Vancouver – C$6,151 Waterloo - C$6,151
+         *
+         */
+    }
+
+}
